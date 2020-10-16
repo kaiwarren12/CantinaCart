664c96c2ce67fbea4ed4c95d8b0e79187ed6cffd
Christian Faatoafe <chris_sila39@hotmail.com>
2020-10-15 17:58:13 -0800

    added api and event listener

--- js/script.js
+++ js/script.js
@@ -15,4 +15,26 @@ function record() {
     }
     recognition.start();
 }
-    // end of voice recognition(Merdad)
+// end of voice recognition(Merdad)
+
+$("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+
+    var name = $("#nameInput").val();
+    var queryUrl = "https://api.agify.io?name=" + name;
+    var queryUrl2 = "https://api.nationalize.io?name=" + name;
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+
+    $.ajax({
+        url: queryUrl2,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    })
+})
