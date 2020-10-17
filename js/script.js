7ae57f0bbeb3a25a3323ae6bbe8d49f34806c866
Christian Faatoafe <chris_sila39@hotmail.com>
2020-10-17 10:54:09 -0800

    added results table

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-$("#searchBtn").on("click", function (event) {
-    event.preventDefault();
-
-    var name = $("#nameInput").val();
-    var queryUrl = "https://api.agify.io?name=" + name;
-    var queryUrl2 = "https://api.nationalize.io?name=" + name;
-
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    });
-
-    $.ajax({
-        url: queryUrl2,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    })
-})
 // added this code for the result function in HTML (Merdad)
 $(function () {
     $('[data-callout-hover-reveal]').hover(function () {
